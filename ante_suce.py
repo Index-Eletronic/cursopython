2973f1bfdac4ca69e4d943279a859df30f1c4316
ftuon <index.eletronic@gmail.com>
2021-02-01 14:39:19 -0300

    .
    Signed-off-by: ftuon <index.eletronic@gmail.com>

---
+++ ante_suce.py
@@ -0,0 +1,8 @@
+#Desafio: Faça um programa que leia um numero inteiro e mostre na tela o seu sucessor e seu antecessor.
+
+n1 = int(input('Digite o numero aqui !!'))
+ant = int(n1-1)
+suc = int(n1+1)
+print('O numero digitado é: {}''\n E seu antecessor è: {}''\n E seu sucessor é: {}'.format(n1, ant, suc))
+
+
