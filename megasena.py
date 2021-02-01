347c15a653abd979b0e712dfcdb281dfc09f8ceb
ftuon <index.eletronic@gmail.com>
2021-02-01 16:56:53 -0300

    .
    Signed-off-by: ftuon <index.eletronic@gmail.com>

--- megasena.py
+++ megasena.py
@@ -7,5 +7,7 @@ num3 = random.randint(21, 30)
 num4 = random.randint(31, 40)
 num5 = random.randint(41, 50)
 num6 = random.randint(51, 60)
+nome = str(input('Digite seu nome: '))
 
+print('Hoje é seu dia de sorte {} seu jogo é: '.format(nome))
 print('Jogo 1: {} {} {} {} {} {}'.format(num1, num2, num3, num4, num5, num6))
