362f71ee8422bdb47bba43ac19bffe92246c5bb4
ftuon <index.eletronic@gmail.com>
2021-02-01 16:54:01 -0300

    .
    Signed-off-by: ftuon <index.eletronic@gmail.com>

---
+++ megasena.py
@@ -0,0 +1,11 @@
+#Jogos da mega sena.
+import random
+
+num1 = random.randint(1, 10)
+num2 = random.randint(11, 20)
+num3 = random.randint(21, 30)
+num4 = random.randint(31, 40)
+num5 = random.randint(41, 50)
+num6 = random.randint(51, 60)
+
+print('Jogo 1: {} {} {} {} {} {}'.format(num1, num2, num3, num4, num5, num6))
--- modulos.py
+++ modulos.py
@@ -1,4 +1,5 @@
-import math
+import math # import
+# from math import sqrt = irá importa somente a função sqrt
 num = int(input('Digite um numero'))
 raiz = math.sqrt(num)
 print('A raiz de {} é igual {}'. format(num, math.ceil(raiz)))
