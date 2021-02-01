e556755c0914ba1abae306c23715e9bb23bce03d
ftuon <index.eletronic@gmail.com>
2021-02-01 13:47:22 -0300

    add new line

--- curso.py
+++ curso.py
@@ -1,2 +1,9 @@
-print ('helo')
-print('Welcome Filipe')
+# Operadores Aritiméticos
+# 5**2 = potencia -> 5x5
+#5 // 2 = Divisão inteira ->2
+#5 % 2 = Resto da divisão -> 1
+
+# Ordem de precedencia: 1( ), 2**, 3 * / // %, 4 + -.
+
+
+
