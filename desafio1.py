c62e2f00feade2f90876028c90798d8d15d63211
ftuon <index.eletronic@gmail.com>
2021-02-02 17:00:41 -0300

    .
    Signed-off-by: ftuon <index.eletronic@gmail.com>

--- desafio1.py
+++ desafio1.py
@@ -28,14 +28,37 @@ tangente = tan(radians(angulo))
 print('o angulo de {} tem a TANGENTE de {;.2f}'.format(angulo, tangente))'''
 #==========================================================================================================================================================
 #Um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele, lendo o nome dos alunos e escrevendo na tela o nome do escolhido.
-from random import choice
+'''from random import choice
 n1 = str(input('Primeiro aluno: '))
 n2 = str(input('Segundo aluno: '))
 n3 = str(input('Terceiro aluno: '))
 n4 = str(input('Quarto aluno'))
 lista = [n1, n2, n3, n4]
 escolhido = choice(lista)
-print('O aluno escolhido foi {}'.format(escolhido))
+print('O aluno escolhido foi {}'.format(escolhido))'''
+#==========================================================================================================================================================
+#O mesmo professor do desafio 19 quer sortear a ordem de apresentação de trabalhos dos alunos. Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
+'''import random
+n1 = str(input('Primeiro aluno: '))
+n2 = str(input('Segundo aluno: '))
+n3 = str(input('Terceiro aluno: '))
+n4 = str(input('Quarto aluno'))
+lista = [n1, n2, n3, n4]
+random.shuffle(lista)
+print('A ordem de apresentação será:')
+print(lista)'''
+#==========================================================================================================================================================
+#Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3.
+import pygame
+pygame.init()
+pygame.mixer.music.load('teste.mp3')
+pygame.mixer.music.play()
+pygame.event.wait()
+
+
+
+
+
 
 
 
